fbec8f9d7c480d70c11f44d5ea204d06a37888e5
yutarot <takenoco88@gmail.com>
2018-03-31 08:20:01 +0900

    add build profiles

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 const path = require('path');
 const dist = path.resolve(__dirname, 'dist');
+const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
+let loaders = [];
+
+loaders.push({
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['babel-preset-es2015']
+    }
+  }
+});
+
+let config = {
   mode: 'production',
   entry: './src/js/app.js',
   output: {
     path: dist,
     filename: 'bundle.js'
   },
-  devtool: 'source-map',
   resolve: {
     extensions: ["*", ".webpack.js", ".web.js", ".js"]
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-es2015']
-        }
-      }
-    }]
+    rules: loaders
   }
 };
+
+if (devMode) {
+  config.devtool = 'source-map';
+}
+
+module.exports = config;
