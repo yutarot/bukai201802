c934afcd6136429a4c200039a4cc7c0a1c031635
yutarot <takenoco88@gmail.com>
2018-03-31 20:32:01 +0900

    add split process for webpack build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const $ = require('jquery');
-const Chart = require('chart.js');
-const Constants = require('./constants');
+import $ from 'jquery';
+import Chart from 'chart.js';
+import constants from './constants';
+import util from './util';
 
-(function() {
+(function () {
   "use strict";
 
   var $window = $(window);
@@ -10,77 +11,18 @@ const Constants = require('./constants');
   var $questions = $('#questions');
   var $warning = $('#warning');
   var $result = $('#result');
-  var pointValues = Constants.getPointValues();
-  var chartConfig = Constants.getChartConfig();
+  var pointValues = constants.pointValues;
+  var chartConfig = constants.chartConfig;
 
-  /**
-   * Calculate points based on the user's answers
-   * @return {array} array which contains each result of points
-   */
-  var calcPoints = function() {
-    var ret = [0, 0, 0, 0, 0, 0];
-    $('input:checked').each(function(i, elem) {
-      $.each(pointValues[$(elem).attr('id')], function(idx, val) {
-        ret[idx] += val;
-      });
-    });
-    return ret;
-  };
-
-  /**
-   * Set points to the result table and show it.
-   * @param  {array} data array contains each points
-   */
-  var setPoints = function(data) {
-    var strengthCamera, strengthThreeDee, strengthFantasy, strengthDictionary, strengthRadio, strengthSound;
-    $('#camera').text(data[0]);
-    $('#threeDee').text(data[1]);
-    $('#fantasy').text(data[2]);
-    $('#dictionary').text(data[3]);
-    $('#radio').text(data[4]);
-    $('#sound').text(data[5]);
-
-    strengthCamera = data[0] > 25 ? Constants.excellent() : data[0] > 14 ? Constants.good() : Constants.poor();
-    strengthThreeDee = data[1] > 25 ? Constants.excellent() : data[1] > 14 ? Constants.good() : Constants.poor();
-    strengthFantasy = data[2] > 25 ? Constants.excellent() : data[2] > 14 ? Constants.good() : Constants.poor();
-    strengthDictionary = data[3] > 25 ? Constants.excellent() : data[3] > 14 ? Constants.good() : Constants.poor();
-    strengthRadio = data[4] > 25 ? Constants.excellent() : data[4] > 14 ? Constants.good() : Constants.poor();
-    strengthSound = data[5] > 25 ? Constants.excellent() : data[5] > 14 ? Constants.good() : Constants.poor();
-
-    $('#strengthCamera').text(strengthCamera);
-    $('#strengthThreeDee').text(strengthThreeDee);
-    $('#strengthFantasy').text(strengthFantasy);
-    $('#strengthDictionary').text(strengthDictionary);
-    $('#strengthRadio').text(strengthRadio);
-    $('#strengthSound').text(strengthSound);
-  };
-
-  /**
-   * Check and return the invalid element.
-   * @return {object} HTML element of not answered question
-   */
-  var checkInvalidInput = function() {
-    var ret;
-    $('.question').each(function(i) {
-      var checkedElement = $(this).find('input:checked');
-      if (checkedElement.length === 0) {
-        ret = this;
-        // stop the loop
-        return false;
-      }
-    });
-    return ret;
-  };
-
-  // Check the answers and show the result
-  $('#submitButton').on('click', function(e) {
-    var invalidElement = checkInvalidInput();
+  // submit button on the test view
+  $('#submitButton').on('click', function (e) {
+    var invalidElement = util.checkInvalidInput();
     if (invalidElement) {
       // scroll and blink the invalid question
       var $invalidElement = $(invalidElement);
       var invalidPosition = $invalidElement.offset().top;
       var scrollNeeded = invalidPosition < $window.scrollTop();
-      var blinkTitle = function() {
+      var blinkTitle = function () {
         // blink the title 3 times
         $invalidElement.find('.question_title')
           .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -90,27 +32,30 @@ const Constants = require('./constants');
           scrollTop: invalidPosition - 10
         }, 2000, blinkTitle);
       } else {
+        // immidiately blinking the title when the elemnt is already in the window
         blinkTitle();
       }
     } else {
       // show the result chart radar
       var $chart = $('#myChart');
-      chartConfig.data.datasets[0].data = calcPoints();
+      chartConfig.data.datasets[0].data = util.calcPoints();
       var myRadar = new Chart($chart, chartConfig);
-      setPoints(chartConfig.data.datasets[0].data);
-      $questions.fadeOut(500, function() {
+      util.setPoints(chartConfig.data.datasets[0].data);
+      $questions.fadeOut(500, function () {
         $result.fadeIn(500);
       });
     }
   });
 
-  $('#backButton').on('click', function(e) {
-    $result.fadeOut(500, function() {
+  // back button on the result view
+  $('#backButton').on('click', function (e) {
+    $result.fadeOut(500, function () {
       $questions.fadeIn(500);
     });
   });
 
-  $('#acceptWarning').on('click', function(e) {
+  // accept button on the initial view
+  $('#acceptWarning').on('click', function (e) {
     $warning.hide();
     $questions.show();
   });
--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,24 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const dist = path.resolve(__dirname, 'dist');
 
+var webpack = require('webpack');
+
 module.exports = {
   output: {
-    path: dist,
-    filename: 'bundle.js'
+    filename: '[name].bundle.js',
+    path: dist
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+    }
   },
   resolve: {
     extensions: ["*", ".webpack.js", ".web.js", ".js"]
   },
   plugins: [
-    new CleanWebpackPlugin(dist),
+    new CleanWebpackPlugin(dist)
   ],
   module: {
     rules: [{
