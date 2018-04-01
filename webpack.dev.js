065b34af3c279ed880a4fa7ccbfc6b1c9a57acfa
yutarot <takenoco88@gmail.com>
2018-04-01 20:30:21 +0900

    add debug.js for development

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,10 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    entry: {
+        main: './src/index.js',
+        debug: './src/debug.js'
+    },
     devtool: 'source-map',
     module: {
         rules: [{
