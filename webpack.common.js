f21dd84fff0a8bbc473c1f2ad2d0891c475c1620
yutarot <takenoco88@gmail.com>
2018-04-20 19:20:43 +0900

    replace babel-preset-es2015 with babel-preset-env

--- webpack.common.js
+++ webpack.common.js
@@ -35,10 +35,7 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-es2015']
-        }
+        loader: 'babel-loader'
       }
     },
     {
