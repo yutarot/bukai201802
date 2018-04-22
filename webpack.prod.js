7917f93a4a9e8d7632338aab689a062680196daa
yutarot <takenoco88@gmail.com>
2018-04-22 09:20:38 +0900

    fix minimize configurations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,29 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const LicenseInfoWebpackPlugin = require('license-info-webpack-plugin').default;
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true
+                    },
+                    output: {
+                        ascii_only: true,
+                        comments: /^\**!|@preserve|@license|@cc_on/,
+                    }
+                },
+            }),
+        ],
+    },
     plugins: [
-        new UglifyJSPlugin()
+        new LicenseInfoWebpackPlugin({
+            glob: '{LICENSE,license,License}*',
+        }),
     ],
     module: {
         rules: []
